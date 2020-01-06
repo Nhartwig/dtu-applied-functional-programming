ae0c4aa5bb33fed57e47afc029058030f343da99
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2020-01-06 11:26:47 +0100

    Code translated from SML

---
+++ Project1/translated.fsx
@@ -0,0 +1,60 @@
+(* 2 Representing trees *)
+type Tree<'a> = Node of 'a * ('a Tree list)
+
+let movetree (Node((label, x), subtrees), x') = 
+    Node((label, x+x'), subtrees)
+
+(* 3 representing extents *)
+type Extent = (float * float) list
+
+let moveextent (e : Extent, x) = List.map (fun (p,q) -> (p+x, q+x) ) e
+
+let rec merge (ps : Extent) (qs : Extent) =
+    match (ps, qs) with
+    | (_, []) -> ps
+    | ([], _) -> qs
+    | ((p,_)::ps', (_,q)::qs') -> (p,q) :: merge ps' ps'
+
+let mergelist (es : Extent list) = List.fold merge es []
+
+(* 4 Fitting extents *)
+let rmax (p, q) = if p > q then p else q
+let rec fit (ps :Extent) (qs : Extent) = 
+    match (ps, qs) with
+    | ((_,p)::ps, (q,_)::qs) -> rmax(fit ps qs, p - q + 1.0)
+    | (_,_) -> 0.0
+
+let fitlist1 es =
+    let rec fitlist1' acc es =
+        match es with
+        | [] -> []
+        | e::es' -> 
+            let x = fit acc e
+            x :: fitlist1' (merge acc (moveextent(e, x) )) es'
+    fitlist1' [] es
+
+let fitlistr es =
+    let rec fitlistr' acc es =
+        match es with
+        | [] -> []
+        | e::es' -> 
+            let x = -(fit acc e)
+            x :: fitlistr' (merge (moveextent(e,x)) acc) es'
+    List.rev (fitlistr' [] (List.rev es))
+
+let mean (x,y) = x+y/2.0
+let fitlist (es: Extent list) = List.map mean (List.zip (fitlist1 es) (fitlistr es))
+
+(* 5 Designing the tree *)
+
+let design tree =
+    let rec design' (Node(label, subtrees)) =
+        let trees, extents = List.unzip (List.map design' subtrees)
+        let positions = fitlist extents
+        let ptrees = List.map movetree (List.zip trees positions)
+        let pextents = List.map moveextent (List.zip extents positions)
+        let resultextent = (0.0, 0.0) :: mergelist pextents
+        let resulttree = Node((label, 0.0), ptrees)
+        (resulttree, resultextent)
+    fst (design' tree)
+
