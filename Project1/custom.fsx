683c541ffe0b5c7cf1803c0712ee0e6fcff60823
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2020-01-07 11:21:19 +0100

    string concat and time testing

--- Project1/custom.fsx
+++ Project1/custom.fsx
@@ -2,6 +2,8 @@
 open Translated
 #load "postscript.fsx"
 open Postscript
+#load "Timing.fsx"
+open Timing
 (* Our own part *)
 
 //let test = design(Node(1,[Node(2,[Node(4,[]); Node(5,[]); Node(6,[]); Node(7,[])]); Node(3,[]); Node(9,[])]))
@@ -17,16 +19,23 @@ let randomtree l =
     Node(sprintf "%i" 0,randomtree' 0)
 //printf "%A" (randomtree 5)
 
-
-
-// Testing postscript
+// Defining objects
 let rec scaletree n (Node(x', y)) =
     Node((fst x', (snd x') * n), List.map (scaletree n) y)
 
-let tree = (design (randomtree 5)) |> (scaletree 5.0)
+let tree = (design (randomtree 7)) |> (scaletree 5.0)
 
-let postscripttree = (postscript tree)
-printf "%s" postscripttree
+// Time difference
+
+let timeDefault = timeOperation (fun() -> postscript tree)
+let timeConcat = timeOperation (fun() -> postscriptConcat tree)
+
+printf "\n\nTime for default: %i\nTime for Concat:  %i\n\n" (timeDefault.millisecondsTaken) (timeConcat.millisecondsTaken)
+
+// Testing postscript
+
+let postscripttree = (postscriptConcat tree)
+//printf "%s" postscripttree
 
 open System.IO;
 
--- Project1/postscript.fsx
+++ Project1/postscript.fsx
@@ -36,3 +36,41 @@ let postscript tree =
 
     let middle = postscript' (0, 0) tree
     start + middle + ending
+
+let postscriptConcat tree =
+    let start = "%!
+    <</PageSize[1400 1000]/ImagingBBox null>> setpagedevice
+    1 1 scale
+    700 999 translate
+    newpath
+    /Times-Roman findfont 10 scalefont setfont\n"
+    let ending = "\nstroke\nshowpage"
+
+    let positions x (subtree: Tree<string * float> list) = List.map (fun (Node(x', _)) -> x + (int (snd x'))) subtree
+
+    let drawLines pos y =
+        List.collect (fun x ->
+            [sprintf "%i %i moveto" x (y); sprintf "%i %i lineto" x (y - 10)]) pos
+
+    let rec postscript' (x: int, y: int) (Node((label, X'), subtree): Tree<string * float>) =
+        match subtree with
+        | [] ->
+            String.concat "\n" [sprintf "%i %i moveto" ((int X') + x) (y - 10);
+                sprintf "(%s) dup stringwidth pop 2 div neg 0 rmoveto show" label]
+        | _ ->
+            let X = int X'
+            let pos = positions (X + x) subtree
+            let strings = 
+                List.concat [ [sprintf "%i %i moveto" (X + x) (y - 10);
+                    sprintf "(%s) dup stringwidth pop 2 div neg 0 rmoveto show" label;
+                    sprintf "%i %i moveto" (X + x) (y - 13); 
+                    sprintf "%i %i lineto" (X + x) (y - 40);
+                    sprintf "%i %i moveto" (List.min pos) (y - 40); 
+                    sprintf "%i %i lineto" (List.max pos) (y - 40)];
+                    (drawLines pos (y - 40));
+                    //"stroke\n" +
+                    ((List.map (postscript' (X + x, y - 50)) subtree) ) ]
+            String.concat "\n" strings
+
+    let middle = postscript' (0, 0) tree
+    start + middle + ending
