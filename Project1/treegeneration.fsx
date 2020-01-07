c0d96c3856a285ebccdf3dc3954dbfb6401b0604
Nathaniel <hartwig.n@husky.neu.edu>
2020-01-07 16:56:42 +0100

    added square tree generation function

--- Project1/treegeneration.fsx
+++ Project1/treegeneration.fsx
@@ -11,3 +11,15 @@ let randomtree l =
 
 let rec scaletree (n:float) (Node(x', y)) =
     Node((fst x', (snd x') * n), List.map (scaletree n) y)
+
+let willowTree num_levels =
+    let rec willowTree' l level = 
+        match l,level with
+        | _,_ when l=num_levels -> [1..12] |> List.map (fun _ -> Node(sprintf "%i" (num_levels - level), willowTree' 1 (level-1)))
+        | _,_ when level > 0 ->  [Node(sprintf "%i" (num_levels - level), willowTree' 1 (level-1))]
+        | _,_ -> [Node(sprintf "%i" (num_levels - level),[])]
+    Node(sprintf"%i" 0, willowTree' num_levels num_levels)
+
+let squareTree q = 
+    match q with
+    | _ -> Node(sprintf "", [1..q] |> List.map (fun _ -> willowTree q))
