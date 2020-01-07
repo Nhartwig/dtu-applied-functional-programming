c8602b650d5573f551863798dea3a1af0a0048c0
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2020-01-07 22:24:00 +0100

    depth indicating names on randomtree

--- Project1/treegeneration.fsx
+++ Project1/treegeneration.fsx
@@ -6,8 +6,8 @@ let randomtree l =
     let rec randomtree' n =
         match n with
         | _ when n >= r.Next(1,l) -> [Node(sprintf "%i" n,[])]
-        | _ -> [1 .. r.Next(1, l+l)] |> List.map (fun _ -> Node(sprintf "%i" 1, randomtree' (n+1)))
-    Node(sprintf "%i" 0,randomtree' 0)
+        | _ -> [1 .. r.Next(1, l+l)] |> List.map (fun _ -> Node(sprintf "%i" n, randomtree' (n+1)))
+    Node(sprintf "%i" 0,randomtree' 1)
 
 let rec scaletree (n:float) (Node(x', y)) =
     Node((fst x', (snd x') * n), List.map (scaletree n) y)
