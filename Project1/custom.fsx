e477ea208f40af673fff0d32e1f7bdbbf371d093
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2020-01-08 13:12:33 +0100

    adding new lines to postscript string construction functions

--- Project1/custom.fsx
+++ Project1/custom.fsx
@@ -17,9 +17,9 @@ let tree =  randomtree 1
 let trees = treeBatch [100;200;300;400] 
 
 // Time difference
-let timeDefault = timeOperation (fun() -> postscript' (List.fold (fun s x -> s + (x) ) "" ) tree)
+let timeDefault = timeOperation (fun() -> postscript' (List.fold (fun s x -> s + (x) + "\n" ) "" ) tree)
 let timeConcat = timeOperation (fun() -> postscript' (String.concat "\n") tree)
-let stringbuilder = fun xs -> xs |> Seq.fold (fun (sb:System.Text.StringBuilder) (s: string) -> sb.Append(s) ) (System.Text.StringBuilder()) |> fun sb -> sb.ToString()
+let stringbuilder = fun xs -> xs |> Seq.fold (fun (sb:System.Text.StringBuilder) (s: string) -> sb.Append(s).Append("\n") ) (System.Text.StringBuilder()) |> fun sb -> sb.ToString()
 let timeBuilder = timeOperation (fun() -> postscript' (stringbuilder) tree)
 
 printf "\n\nTime for default: %i\nTime for Concat:  %i\nTime for Builder: %i\n\n" (timeDefault.millisecondsTaken) (timeConcat.millisecondsTaken) (timeBuilder.millisecondsTaken)
