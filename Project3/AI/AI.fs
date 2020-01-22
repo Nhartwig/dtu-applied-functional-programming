10365f5668d9c25c1faa34377e0cbbbf932c0f07
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2020-01-22 14:29:52 +0100

    AI chooseMove optimally

--- Project3/AI/AI.fs
+++ Project3/AI/AI.fs
@@ -1,4 +1,8 @@
 module AI
 type Game = int list
-let chooseMove (game:Game) = (1,0)
+let chooseMove (game:Game) = 
+    let m = List.fold (^^^) 0 game
+    if m = 0
+    then (1,List.findIndex (fun x -> x = List.max game) game)
+    else List.findIndex (fun x -> (x ^^^ m) < x) game |> fun i -> (List.item i game - (List.item i game ^^^ m),i)
 let chooseMoveWithHandicap (handicap:int) (game:Game) = chooseMove(game)
