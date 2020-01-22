72cec4ce97f1d7fb819b82db106f84016d60bbf8
Nathaniel <hartwig.n@husky.neu.edu>
2020-01-22 21:00:21 +0100

    enhanced radio getting/setting functions

--- Project3/GUI/GUI.fs
+++ Project3/GUI/GUI.fs
@@ -12,13 +12,6 @@ type Game = int list
 type Difficulty = Novice | Intermediate | Expert
 type ID = int
 
-
-type button = 
-    | NewGame of Button 
-    | Random of Button 
-    | Seed of Button
-    | Quit of Button
-
 type box = 
     | Welcome of string*MessageBox
     | GameBox of int*int*Boolean*GroupBox
@@ -33,95 +26,150 @@ type popup =
     | Outcome of Boolean*Form
 
 
-
-
-
 // Match functions (generate matches, pick/remove/update matches)
-let createMatches = //
-let updateMatches = //
+let createMatches = ()
+
+let updateMatches = ()
 
 
-
-
-
-
-let createSelDifficultyRadios = 
-    let rb1 = new RadioButton((Name="Novice",Location=Point(100,1),MinimumSize=Size(10,10),MaximumSize=Size(10,10),Text="Novice"))
-    let rb2 = new RadioButton((Name="Intermediate",Location=Point(100,1),MinimumSize=Size(10,10),MaximumSize=Size(10,10),Text="Intermediate"))
-    let rb3 = new RadioButton((Name="Expert",Location=Point(100,1),MinimumSize=Size(10,10),MaximumSize=Size(10,10),Text="Expert"))
+let createDifficultyRadios = 
+    let rb1 = new RadioButton(Name="Novice",Location=Point(100,1),MinimumSize=Size(10,10),MaximumSize=Size(10,10),Text="Novice")
+    let rb2 = new RadioButton(Name="Intermediate",Location=Point(100,1),MinimumSize=Size(10,10),MaximumSize=Size(10,10),Text="Intermediate")
+    let rb3 = new RadioButton(Name="Expert",Location=Point(100,1),MinimumSize=Size(10,10),MaximumSize=Size(10,10),Text="Expert")
     [rb1; rb2; rb3]
 
 
-
-let createSelectPlayerRadios = 
-    let playerFirst = new RadioButton((Name="player" ,Location=Point(100,1),MinimumSize=Size(10,10),MaximumSize=Size(10,10),Text="Go First"))
-    let aiFirst = new RadioButton((Name="ai", Location=Point(100,1),MinimumSize=Size(10,10),MaximumSize=Size(10,10),Text="Let AI Go First"))
-    [pFirst; aiFirst]
+let createPlayerRadios = 
+    let playerFirst = new RadioButton(Name="player" ,Location=Point(100,1),MinimumSize=Size(10,10),MaximumSize=Size(10,10),Text="Go First")
+    let aiFirst = new RadioButton(Name="ai", Location=Point(100,1),MinimumSize=Size(10,10),MaximumSize=Size(10,10),Text="Let AI Go First")
+    [playerFirst; aiFirst]
 
 
 let createButtons = 
-    let gamebutton = new NewGame(Location=Point(100,1),MinimumSize=Size(100,50),MaximumSize=Size(100,50),Text="New Game!")
-    let randombutton = new Random(Location=Point(100,1),MinimumSize=Size(100,50),MaximumSize=Size(100,50),Text="Random Game")
-    let seedbutton = new Seed(Location=Point(100,1),MinimumSize=Size(100,50),MaximumSize=Size(100,50),Text="Seed Game from Web")
-    let quitButton = new Quit(Location=Point(100,1),MinimumSize=Size(100,50),MaximumSize=Size(100,50),Text="New Game!")
-    [gamebutton; randombutton; seedbutton; quitButton]
+    let gameButton = new Button(Name="gameButton",Location=Point(100,1),MinimumSize=Size(100,50),MaximumSize=Size(100,50),Text="New Game!")
+    let randomButton = new Button(Name="randomButton",Location=Point(200,1),MinimumSize=Size(100,50),MaximumSize=Size(100,50),Text="Random Game")
+    let seedButton = new Button(Name="seedButton",Location=Point(300,1),MinimumSize=Size(100,50),MaximumSize=Size(100,50),Text="Seed from Web")
+    let startButton = new Button(Name="startButton",Location=Point(400,1),MinimumSize=Size(100,50),MaximumSize=Size(100,50),Text="Start Game!")
+    let quitButton = new Button(Name="quitButton", Location=Point(500,1),MinimumSize=Size(100,50),MaximumSize=Size(100,50),Text="Quit Game ;(")
+    [gameButton; randomButton; seedButton; startButton; quitButton]
 
-let createWindow = 
-    let window = new MainWindow(Text="Nim Game", Size=Size(800,800))
-    window
+let createWindow str x y = 
+    match str, x, y with
+    | str, x, y when (((x > 0) && (y > 0)) && ((String.length (str)) > 0))-> new Form(Text=str, Size=Size(x,y))
+    | _,_,_ -> new Form(Text="Nim Game", Size=Size(800,800))
+
 
 let buttonList = createButtons
 
-let addButtons window buttons = List.fold (fun btn window -> window.Controls.Add btn) window buttons
+let addButtons (window:Form) (buttons: Button list) = List.fold (fun (window:Form) btn -> (window.Controls.Add btn); window) window buttons
 
-let controlsMap = Map.ofList (List.zip ["gameButton"; "randomButton";"seedButton"; "startPlayer";"quitButton"] (buttonList))
+let controlsMap = Map.ofList (List.zip ["gameButton"; "randomButton";"seedButton"; "startButton";"quitButton"] (buttonList))
 
-let selPlayerRadios = createSelectPlayerRadios
-let selDifficultyRadios = createSelDifficultyRadios 
+let playerRadios = createPlayerRadios
 
+let difficultyRadios = createDifficultyRadios 
 
+// getDiff and getPlayer can handle empty/ bad selections by setting defaults
+let rec getRadioSelection (radios:RadioButton list) : string = 
+    match radios with
+    | [] -> ""
+    | r::rs -> if r.Checked then (r.Name) else (getRadioSelection rs)
 
+// set default difficulty to Intermediate if nothing is selected
+let getDifficulty rList =
+    match (rList |> getRadioSelection) with
+    | "Novice" ->           Novice
+    | "Intermediate" ->     Intermediate
+    | "Expert" ->           Expert
+    | _        ->           Intermediate
 
-let getRadioSelection (radios:RadioButton list) : string = 
-    let (res:RadioButton) = (List.tryFind (radio.Checked = true) radios)
-    res.Name
+// set default player1 to player (user)
+let getPlayer rList = 
+    match (rList |> getRadioSelection) with
+    | "player"  ->  Player 
+    | "ai"      ->  PC
+    | _     ->      Player 
 
 // Initialization
 
-let window = createWindow
+let window = createWindow " " 0 0
 
 let initialize() = 
-    (window buttonList |> addButtons)
+    let window = addButtons window buttonList
 
-    window.Controls.Add (selPlayerBox)
-    window.Controls.Add (selDifficultyBox)
-    controlsMap.Add ("selPlayerBox", selPlayerBox)
-    controlsMap.Add ("selDifficultyBox", selDifficultyBox)
+    let difficultyGroupBox = new GroupBox(Name="difficultyGroupBox",AutoSize=true)
+    List.iter (difficultyGroupBox.Controls.Add) difficultyRadios
+
+    let playerGroupBox = new GroupBox(Name="playerGroupBox",AutoSize=true)
+    List.iter (playerGroupBox.Controls.Add) playerRadios
+
+    window.Controls.Add difficultyGroupBox
+    window.Controls.Add playerGroupBox
     ()
 
+let randomGame (func: (unit-> unit)) = (controlsMap.Item "randomButton").Click.Add (fun _ -> func())
 
-val randomGame : (unit -> unit) -> unit
-let randomGame (func: (unit-> unit)) = (controlsMap.TryFind "randomButton").Click.Add (fun _ -> func())
+let loadGame (func:(string-> unit)) = (controlsMap.Item "seedButton").Click.Add (fun _ -> func("www.google.dk")) 
 
-val loadGame : (string -> unit) -> unit
-let loadGame (func:(str-> unit)) = (controlsMap.TryFind "loadButton").Click.Add (fun str -> func(str)) 
-
-val startGame : (Player -> Difficulty -> unit) -> unit
 let startGame (func: (Player -> Difficulty -> unit)) = 
+    let p = (getPlayer playerRadios)
+    let diff = (getDifficulty difficultyRadios)
+    (controlsMap.Item "startButton").Click.Add (fun _ -> func p diff)
+    ()
 
-val chooseMove : (int * int -> unit) -> unit
-val cancel : (unit -> unit) -> unit
-val restart : (unit -> unit) -> unit
+// Need to implement more comprehensive choosing function here
+let chooseMove (func: (int * int -> unit)) = (controlsMap.Item "gameButton").Click.Add (fun _ -> func(1,0))
+
+
+let rec clearRadios (rList:RadioButton list) =
+    match rList with
+    | [] -> rList
+    | r::rs -> (r |> (fun r -> (r.Checked <- false);r))::(clearRadios rs)   
+
+let rec disableRadios (rList:RadioButton list) = 
+    match rList with 
+    | [] -> rList
+    | r::rs -> (r|> (fun r -> (r.Enabled <- false); r))::(disableRadios rs)
+
+let rec enableRadios (rList: RadioButton list) = 
+    match rList with
+    | [] -> rList
+    | r::rs -> (r|> (fun r -> (r.Enabled <- true);r))::(enableRadios rs)
+
+let rec disableButtons (bList: Button list) = 
+    match bList with
+    | [] -> bList
+    | b::bs -> (b|> (fun b -> (b.Enabled <- false);b))::(disableButtons bs)
+
+let rec enableButtons (bList: Button list) = 
+    match bList with
+    | [] -> bList
+    | b::bs -> (b|> (fun b -> (b.Enabled <- true);b))::(enableButtons bs)
+
+
+let cancel (func:(unit -> unit)) = ()
+
+let restart (func:(unit->unit)) = ()
+
+
+
 
 // Show GUI
-
 let show() = Application.Run(window)
+
+
+
+
 
 // Interactions
 let taunt str = 
     match str with
     | str ->    let popup = new Form(Text=str, Size=Size(300,300))
                 Application.Run(popup)
+
+
+
+
 
 // State changes
 val init : unit -> unit
