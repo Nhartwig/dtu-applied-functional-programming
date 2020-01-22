c30fd5c79de0fee9428a864e74633ac6d10b96c4
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2020-01-22 13:09:11 +0100

    initial basic nothing

---
+++ Project3/GUI/BasicGUI.fs
@@ -0,0 +1,49 @@
+module GUI
+
+open System.Windows.Forms 
+open System.Drawing 
+
+type Player = Player | PC
+type Game = int list
+type Difficulty = Novice | Intermediate | Expert
+
+// The window part
+let window =
+  new Form(Text="Nim", Size=Size(500,200))
+
+let randomButton =
+    new Button(Location=Point(10,10),Size=Size(235,25), Text="Random")
+
+let loadButton =
+    new Button(Location=Point(245,10),Size=Size(235,25), Text= "Load")
+
+let disable bs = 
+    for b in [randomButton;loadButton] do 
+        b.Enabled  <- true
+    for (b:Button) in bs do 
+        b.Enabled  <- false
+
+// Initialization
+let initialize() = ()
+let randomGame (f: (unit -> unit)) = ()
+let loadGame (f: (int -> unit)) = ()
+let startGame (f: (Player -> Difficulty -> unit)) = ()
+let chooseMove (f: (int * int -> unit)) = ()
+let cancel (f: (unit -> unit)) = ()
+let restart (f: (unit -> unit)) = ()
+
+// Show GUI
+let show() = ()
+
+// Interactions
+let taunt (s: string) = ()
+
+// State changes
+let init() = ()
+let generatingRandom() = ()
+let getGame() =()
+let ready (game: Game) = ()
+let inProgressC (game: Game) = ()
+let inProgressP (game: Game) = ()
+let moving (game: Game) = ()
+let finish (player: Player) = ()
--- Project3/dllify.sh
+++ Project3/dllify.sh
@@ -1,2 +1,3 @@
 fsharpc -a GameLogic/GameLogic.fsi GameLogic/GameLogic.fs
 fsharpc -a AI/AI.fsi AI/AI.fs
+fsharpc -a GUI/GUI.fsi GUI/BasicGUI.fs -o:GUI.dll
